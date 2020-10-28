8d938f5a279404390b024c107605760c1c5e4d91
ali198724 <ali198724@gmail.com>
2020-10-28 08:18:55 +0300

    add texteditor code

--- source.cpp
+++ source.cpp
@@ -1,4 +1,6 @@
 #include <dali-toolkit/dali-toolkit.h>
+#include <dali-toolkit/devel-api/controls/text-controls/text-field-devel.h>
+#include <dali-toolkit/devel-api/controls/text-controls/text-editor-devel.h>
 
 using namespace Dali;
 using namespace Dali::Toolkit;
@@ -17,13 +19,37 @@ public:
     Window window = application.GetWindow();
     window.SetBackgroundColor(Vector4(0.04f, 0.345f, 0.392f, 1.0f));
 
-    TextField field = TextField::New();
+    mEditor = TextEditor::New();
+    mEditor.SetProperty(Actor::Property::PARENT_ORIGIN, ParentOrigin::TOP_CENTER);
+    mEditor.SetProperty(Actor::Property::ANCHOR_POINT, AnchorPoint::TOP_CENTER);
+    mEditor.SetProperty(Actor::Property::POSITION, Vector3(0.f, 0.0f, 0.f));
+    mEditor.SetProperty(Actor::Property::SIZE, Vector2(100.f, 100.0f));
+    mEditor.SetProperty(TextEditor::Property::TEXT, "Hello\nHello\nHello\nHello\nHello\nHello");
 
-    window.Add(field);
+    mButton = PushButton::New();
+    mButton.SetProperty(Actor::Property::PARENT_ORIGIN, ParentOrigin::CENTER);
+    mButton.SetProperty(Actor::Property::ANCHOR_POINT, AnchorPoint::CENTER);
+    mButton.SetProperty(Actor::Property::SIZE, Vector2(50.f, 50.0f));
+    mButton.SetProperty(Actor::Property::POSITION, Vector3(0.f, 0.0f, 0.f));
+    mButton.SetProperty(Button::Property::LABEL, "click");
+    mButton.ClickedSignal().Connect(this, &SimpleApp::OnButtonClicked);
+
+    window.Add(mButton);
+    window.Add(mEditor);
+  }
+
+  bool OnButtonClicked(Button button)
+  {
+    if(button == mButton)
+    {
+    }
+    return true;
   }
 
 private:
   Application& mApplication;
+  TextEditor mEditor;
+  PushButton mButton;
 };
 
 int DALI_EXPORT_API main(int argc, char** argv)
